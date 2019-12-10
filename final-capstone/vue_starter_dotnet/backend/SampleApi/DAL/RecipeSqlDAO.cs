685e148788d23f532833e86f2d8ce9b884f0dec5
Josh Ward <iceray1895@gmail.com>
2019-12-10 11:34:14 -0500

    Get all recipes and add recipe added untested

--- final-capstone/vue_starter_dotnet/backend/SampleApi/DAL/RecipeSqlDAO.cs
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/DAL/RecipeSqlDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -19,7 +20,34 @@ namespace SampleApi.DAL
 
         public bool CreateRecipe(Recipe newRecipe)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO recipe(name, instructions, vegan, vegetarian, gluten_free, cook_time_in_mins, prep_time_in_mins, serves, difficulty, category)" +
+                        " VALUES (@Name, @IN, @Vegan, @VG, @GF, @CT, @PT, @Serves, @Difficulty, @Category)", conn);
+                    cmd.Parameters.AddWithValue("@Name", newRecipe.Name);
+                    cmd.Parameters.AddWithValue("@IN", newRecipe.Instructions);
+                    cmd.Parameters.AddWithValue("@Vegan", newRecipe.Vegan);
+                    cmd.Parameters.AddWithValue("@VG", newRecipe.Vegetarian);
+                    cmd.Parameters.AddWithValue("@GF", newRecipe.GlutenFree);
+                    cmd.Parameters.AddWithValue("@CT", newRecipe.CookTime);
+                    cmd.Parameters.AddWithValue("@PT", newRecipe.PrepTime);
+                    cmd.Parameters.AddWithValue("@Serves", newRecipe.Servings);
+                    cmd.Parameters.AddWithValue("@Difficulty", newRecipe.Difficulty);
+                    cmd.Parameters.AddWithValue("@Category", newRecipe.Category);
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool DeleteRecipe(Recipe recipe)
@@ -38,7 +66,7 @@ namespace SampleApi.DAL
         /// <returns></returns>
         public IList<Recipe> GetAllRecipes()
         {
-            IList<Recipe> recipes = new Recipe();
+            IList<Recipe> recipes = new List<Recipe>();
 
             try
             {
@@ -46,13 +74,29 @@ namespace SampleApi.DAL
                 {
                     conn.Open();
 
-                    SqlCommand cmd = new SqlCommand("", conn);
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM recipe", conn);
 
                     SqlDataReader reader = cmd.ExecuteReader();
 
                     while (reader.Read())
                     {
-                        recipe.Add(RowToRecipe(reader));
+                        Recipe recipe = RowToRecipe(reader);
+
+                        SqlCommand getIng = new SqlCommand(@"
+                            SELECT ingredient.id as id, ingredient.name as ingname, ingredient_recipe.quantity as quan, ingredient_recipe.unit_of_measurement as unit FROM recipe
+                            JOIN ingredient_recipe ON recipe.id = ingredient_recipe.recipe_id
+                            JOIN Ingredient ON Ingredient.id = ingredient_recipe.ingredient_id
+                            WHERE recipe.id = @Recipe", conn);
+                        cmd.Parameters.AddWithValue("@Recipe", recipe.Id);
+
+                        SqlDataReader ingReader = getIng.ExecuteReader();
+
+                        while (ingReader.Read())
+                        {
+                            recipe.Ingredients.Add(RowToIngredient(ingReader));
+                        }
+
+                        recipes.Add(recipe);
                     }
                 }
             }
@@ -92,5 +136,37 @@ namespace SampleApi.DAL
         {
             throw new NotImplementedException();
         }
+
+        private Recipe RowToRecipe(SqlDataReader reader)
+        {
+            return new Recipe()
+            {
+                Id = Convert.ToInt32(reader["id"]),
+                Name = Convert.ToString(reader["name"]),
+                Author = Convert.ToString(reader["author"]),
+                Instructions = Convert.ToString(reader["intructions"]),
+                Vegan = Convert.ToBoolean(reader["vegan"]),
+                Vegetarian = Convert.ToBoolean(reader["vegetarian"]),
+                GlutenFree = Convert.ToBoolean(reader["gluten_free"]),
+                CookTime = Convert.ToInt32(reader["cook_time_in_mins"]),
+                PrepTime = Convert.ToInt32(reader["prep_time_in_mins"]),
+                Servings = Convert.ToInt32(reader["serves"]),
+                Difficulty = Convert.ToString(reader["difficulty"]),
+                Category = Convert.ToString(reader["category"]),
+            };
+
+        }
+
+        private Ingredient RowToIngredient(SqlDataReader ingReader)
+        {
+            return new Ingredient()
+            {
+                Id = Convert.ToInt32(ingReader["id"]),
+                Name = Convert.ToString(ingReader["ingname"]),
+                Quantity = Convert.ToDouble(ingReader["quan"]),
+                UnitOfMeasurement = ingReader["unit"] == null ? "" : Convert.ToString(ingReader["unit"])
+            };
+        }
+
     }
 }
