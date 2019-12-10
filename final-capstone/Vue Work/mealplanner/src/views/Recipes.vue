4ac7e254c8ae05e23998108ef6385810ac4653a4
andymiller2886 <55465693+andymiller2886@users.noreply.github.com>
2019-12-10 16:03:58 -0500

    Working on Vue

---
+++ final-capstone/Vue Work/mealplanner/src/views/Recipes.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<div id="recipes">
+  {{ recipe }} 
+</div>
+  <h1>Text</h1>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data () {
+    return {
+      recipe: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://localhost:44392/api/meal')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<style>
+
+</style>
